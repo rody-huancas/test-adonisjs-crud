213744003b03fc2ea08163f693d06949ee726eb1
Rody Huancas <rodyhuancas.04@gmail.com>
2026-05-16 19:12:14 -0500

    feat: add app configuration, env schema and service providers

---
+++ start/env.ts
@@ -0,0 +1,23 @@
+import { Env } from '@adonisjs/core/env'
+
+export default await Env.create(new URL('../', import.meta.url), {
+    // Node
+  NODE_ENV : Env.schema.enum(['development', 'production', 'test'] as const),
+  PORT     : Env.schema.number(),
+  HOST     : Env.schema.string({ format: 'host' }),
+  LOG_LEVEL: Env.schema.string(),
+
+    // App
+  APP_KEY: Env.schema.secret(),
+  APP_URL: Env.schema.string({ format: 'url', tld: false }),
+
+    // Session
+  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database'] as const),
+
+    // Database
+  DB_HOST    : Env.schema.string({ format: 'host' }),
+  DB_PORT    : Env.schema.number(),
+  DB_USER    : Env.schema.string(),
+  DB_PASSWORD: Env.schema.string(),
+  DB_DATABASE: Env.schema.string(),
+})
