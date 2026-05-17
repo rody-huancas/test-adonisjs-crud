e528ae9613158ef7d7aa77dc571fac0d1d509a5c
Rody Huancas <rodyhuancas.04@gmail.com>
2026-05-16 20:46:26 -0500

    feat: add CRUD operations for categories and products in routes definition

--- .adonisjs/server/routes.d.ts
+++ .adonisjs/server/routes.d.ts
@@ -8,17 +8,49 @@ export type ScannedRoutes = {
     'auth.access_tokens.store': { paramsTuple?: []; params?: {} }
     'profile.profile.show': { paramsTuple?: []; params?: {} }
     'profile.access_tokens.destroy': { paramsTuple?: []; params?: {} }
+    'profile.categories.index': { paramsTuple?: []; params?: {} }
+    'profile.categories.store': { paramsTuple?: []; params?: {} }
+    'profile.categories.show': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.categories.update': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.categories.destroy': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.index': { paramsTuple?: []; params?: {} }
+    'profile.products.store': { paramsTuple?: []; params?: {} }
+    'profile.products.show': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.update': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.destroy': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
   }
   GET: {
     'profile.profile.show': { paramsTuple?: []; params?: {} }
+    'profile.categories.index': { paramsTuple?: []; params?: {} }
+    'profile.categories.show': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.index': { paramsTuple?: []; params?: {} }
+    'profile.products.show': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
   }
   HEAD: {
     'profile.profile.show': { paramsTuple?: []; params?: {} }
+    'profile.categories.index': { paramsTuple?: []; params?: {} }
+    'profile.categories.show': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.index': { paramsTuple?: []; params?: {} }
+    'profile.products.show': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
   }
   POST: {
     'auth.new_account.store': { paramsTuple?: []; params?: {} }
     'auth.access_tokens.store': { paramsTuple?: []; params?: {} }
     'profile.access_tokens.destroy': { paramsTuple?: []; params?: {} }
+    'profile.categories.store': { paramsTuple?: []; params?: {} }
+    'profile.products.store': { paramsTuple?: []; params?: {} }
+  }
+  PUT: {
+    'profile.categories.update': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.update': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+  }
+  PATCH: {
+    'profile.categories.update': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.update': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+  }
+  DELETE: {
+    'profile.categories.destroy': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
+    'profile.products.destroy': { paramsTuple: [ParamValue]; params: {'id': ParamValue} }
   }
 }
 declare module '@adonisjs/core/types/http' {
