142c6d1d5d49d7da0d0be88c28f117f5ffffddab
Rody Huancas <rodyhuancas.04@gmail.com>
2026-05-16 20:46:17 -0500

    feat: add categories and products CRUD operations to ApiDefinition

--- .adonisjs/client/registry/tree.d.ts
+++ .adonisjs/client/registry/tree.d.ts
@@ -17,5 +17,19 @@ export interface ApiDefinition {
     accessTokens: {
       destroy: typeof routes['profile.access_tokens.destroy']
     }
+    categories: {
+      index: typeof routes['profile.categories.index']
+      store: typeof routes['profile.categories.store']
+      show: typeof routes['profile.categories.show']
+      update: typeof routes['profile.categories.update']
+      destroy: typeof routes['profile.categories.destroy']
+    }
+    products: {
+      index: typeof routes['profile.products.index']
+      store: typeof routes['profile.products.store']
+      show: typeof routes['profile.products.show']
+      update: typeof routes['profile.products.update']
+      destroy: typeof routes['profile.products.destroy']
+    }
   }
 }
